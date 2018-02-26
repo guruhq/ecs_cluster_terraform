f2e82190e2eab692b80b72d2440fe4faffe3e78b
Karl Girthofer <karlgirthofer@gmail.com>
2018-02-26 16:10:50 -0500

    Update cloudwatch.tf

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -37,7 +37,7 @@ resource "aws_cloudwatch_metric_alarm" "container_instance_high_cpu" {
 
 resource "aws_cloudwatch_metric_alarm" "container_instance_low_cpu" {
   alarm_name          = "alarm${var.environment}ClusterCPUReservationLow"
-  comparison_operator = "LessThanxThreshold"
+  comparison_operator = "LessThanThreshold"
   evaluation_periods  = "${var.low_cpu_evaluation_periods}"
   metric_name         = "CPUReservation"
   namespace           = "AWS/ECS"
