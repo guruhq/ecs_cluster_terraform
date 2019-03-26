40d8b599d81b9046ffabdd7a99ef4ddeb60d264c
Karl Girthofer <karlgirthofer@gmail.com>
2019-03-26 13:16:24 -0400

    adding profile

--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "profile" {
+  description = "AWS Profile to use"
+  default     = "default"
+}
+
 variable "project" {
   default = "Unknown"
 }
