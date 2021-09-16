8482d14e6e1769e51363fc3ac23a24e5fb0fb2c0
Shawn Catanzarite <scatanzarite@getguru.com>
2021-09-16 15:56:50 -0700

    cherry pick removing aws provider
    Co-authored-by: Bob Ziuchkovski <bziuchkovski@getguru.com>

--- README.md
+++ README.md
@@ -53,7 +53,6 @@ terraform {
   backend "s3" {
     bucket = "my-terraform-state"
     key    = "dev/terraform.tfstate"
-    region = "us-west-2"
   }
 }
 ```
--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "aws_region" {
-  description = "EC2 Region for the VPC"
-  default     = "us-east-1"
-}
-
 variable "project" {
   default = "Unknown"
 }
